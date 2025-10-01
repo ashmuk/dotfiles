774f09416b940cedbd41f135b0059fdc5aec0eb1
Mukai <hmukai@hmMBPM4.local>
2025-10-01 19:57:37 +0900

    Address shellcheck warning in tmux pane monitor

--- bin/tmux_pane_top.sh
+++ bin/tmux_pane_top.sh
@@ -9,6 +9,7 @@ printf "%-18s %-7s %-5s %-5s %-10s %-6s %-8s %s\n" "PANE" "PID" "CPU%" "MEM%" "E
 # list-panes: PANEID  PID  TTY  CMD  TITLE
 tmux list-panes -aF '#{session_name}:#{window_index}.#{pane_index}	#{pane_pid}	#{pane_tty}	#{pane_current_command}	#{pane_title}' \
 | while IFS=$'\t' read -r pane pid tty cmd title; do
+  : "$cmd" "$title"
   # psでプロセスがまだ居る場合だけ表示
   if psout=$(ps -p "$pid" -o pid=,pcpu=,pmem=,etime=,state=,tty=,comm= 2>/dev/null); then
     read -r pid2 pcpu pmem etime state tty2 comm <<<"$psout"
